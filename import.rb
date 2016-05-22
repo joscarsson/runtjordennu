832df2856d87f6f3287c203f3417e739441487d9
Jonas Oscarsson <jonas.osc@rsson.nu>
2016-05-22 22:09:42 +0200

    Import script to import from Blogger

---
+++ import.rb
@@ -0,0 +1,7 @@
+require "jekyll-import";
+
+JekyllImport::Importers::Blogger.run({
+  "source"                => "blog-05-22-2016.xml",
+  "no-blogger-info"       => true,
+  "replace-internal-link" => false,
+})
