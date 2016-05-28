71b73ef50c4f6d174eb995c4ace50c9444175e5b
Jonas Oscarsson <jonas.osc@rsson.nu>
2016-05-28 21:04:45 +0200

    Upgrade jekyll-import

--- import.rb
+++ import.rb
@@ -4,4 +4,5 @@ JekyllImport::Importers::Blogger.run({
   "source"                => "blog-05-22-2016.xml",
   "no-blogger-info"       => true,
   "replace-internal-link" => false,
+  "comments"              => true,
 })
