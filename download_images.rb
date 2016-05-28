f3e214da0b35b4cd3aa32cd902a842313595749c
Jonas Oscarsson <jonas.osc@rsson.nu>
2016-05-28 18:55:38 +0200

    download_images script as run 'in production'

--- download_images.rb
+++ download_images.rb
@@ -2,43 +2,40 @@ require 'open-uri'
 require 'uri'
 
 def download_image(dir_name, url, src_in_html: false)
-  uri = URI.parse(url)
-  filename = URI.unescape(File.basename(uri.path))
-  return if File.exists?("images/#{dir_name}/#{filename}")
-  Dir.mkdir "images/#{dir_name}" unless File.exists?("images/#{dir_name}")
-  print "Downloading #{filename}: "
-  if src_in_html
-    # When Google puts the actual image in a HTML page...
-    content =  open(url).read
-    actual_url = content.scan(/<img.*?src="(.*?)"/).map { |match| match[0] }[0]
-    open("images/#{dir_name}/#{filename}", "wb") do |file|
-      file << open(actual_url).read
+  begin
+    uri = URI.parse(url)
+    filename = URI.unescape(File.basename(uri.path))
+    return if File.exists?("images/#{dir_name}/#{filename}")
+    Dir.mkdir "images/#{dir_name}" unless File.exists?("images/#{dir_name}")
+    if src_in_html
+      # When Google puts the actual image in a HTML page...
+      content =  open(url).read
+      actual_url = content.scan(/<img.*?src="(.*?)"/).map { |match| match[0] }[0]
+      open("images/#{dir_name}/#{filename}", "wb") do |file|
+        file << open(actual_url).read
+      end
+    else
+      open("images/#{dir_name}/#{filename}", "wb") do |file|
+        file << open(url).read
+      end
     end
-  else
-    open("images/#{dir_name}/#{filename}", "wb") do |file|
-      file << open(url).read
-    end
+  rescue
+    puts "ERROR: Failed to download #{url}!"
   end
-
-  puts "done"
 end
 
 def process_post(post)
   post_name = File.basename(post, '.html')
   puts "Processing #{post_name}"
 
-  if post_name != "2011-03-01-en-avslutande-reflektion-om-indien-och"
-    return
-  end
-
   content = File.read(post)
   img_urls = content.scan(/<img.*?src="(.*?)"/).map { |match| match[0] }
   a_urls = content.scan(/<a.*?href="(.*?)"/).map { |match| match[0] }
-  image_urls = a_urls.select { |url| url.end_with?('.jpg') }
+  image_urls = a_urls.select { |url| url.end_with?('.jpg') or url.end_with?('.png') }
   other_urls = a_urls - image_urls
 
-  puts "Found #{img_urls.length} <img> urls:", img_urls
-  puts "Found #{image_urls.length} <a> urls pointing to images:", image_urls
+  puts "Found #{img_urls.length} <img> urls"
+  puts "Found #{image_urls.length} <a> urls pointing to images"
   puts "Found #{other_urls.length} <a> urls pointing to other things:", other_urls
 
   for url in img_urls
